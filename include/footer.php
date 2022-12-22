1d01413c7b3684afb50a0d46bc083f7f27a3bfe5
thomas-spinec <thomas.spinec@laplateforme.io>
2022-12-22 12:47:42 +0100

    erreur sur le lien du footer , correction

--- include/footer.php
+++ include/footer.php
@@ -12,7 +12,7 @@
                     <a href="https://github.com/thomas-spinec/reservation-salles"><img src="img/github.png" alt="logo github"/></a>
                 </div>
                 <div class="link">
-                    <a href="https://thomas-spinec.students-laplateforme.io/mycms/"
+                    <a href="https://thomas-spinec.students-laplateforme.io/"
                       ><img src="img/computer.png" alt="Lien vers le site"
                     /></a>
                 </div>
